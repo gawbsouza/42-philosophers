ec5a36de631741d8b0c1569ad9a4fca69c10521a
gawbsouza <gabriel.souza@zoho.com>
2023-01-25 09:21:03 -0300

    updadte summary display

--- philo/debug/summary.c
+++ philo/debug/summary.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:53:33 by gasouza           #+#    #+#             */
-/*   Updated: 2023/01/23 21:48:32 by gasouza          ###   ########.fr       */
+/*   Updated: 2023/01/25 09:17:08 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static void	print_philo(t_philo *philo);
 static void	print_configuration(t_simulation *s);
+static void	print_starve_time(t_philo *philo);
 
 void	summary(t_simulation *simulation)
 {
@@ -29,34 +30,55 @@ void	summary(t_simulation *simulation)
 		print_philo(philo);
 		i++;
 	}
+	printf("\n");
 }
 
 static void	print_philo(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->philo_mutex);
+	long	death_time;
+	long	last_meal_time;
+
+	last_meal_time = 0;
+	death_time = 0;
+	if (philo->ate_at)
+		last_meal_time = philo->ate_at - philo->began_at;
+	if (philo->died)
+		death_time = philo->died_at - philo->began_at;
 	printf("Philo: " BLUE "%02ld" RESET " | ", philo->number);
 	if (philo->died)
 		printf(RED "■" RESET " | ");
 	else
 		printf(GREEN "■" RESET " | ");
 	printf("meals: " BLUE "%2d" RESET " | ", philo->meals);
-	printf("began: " BLUE "%13ld" RESET " | ", philo->began_at);
-	printf("last meal: " BLUE "%13ld" RESET " | ", philo->ate_at);
-	printf("died at: " BLUE "%13ld" RESET " | ", philo->died_at);
-	printf("starve time (ms): ");
+	printf("last meal time: " BLUE "%-5ld" RESET " | ", last_meal_time);
+	printf("death time: " BLUE "%-5ld" RESET " | ", death_time);
+	print_starve_time (philo);
+}
+
+static void	print_starve_time(t_philo *philo)
+{
+	printf("starve time: ");
 	if (philo->died && philo->ate_at)
-		printf(BLUE "%ld\n" RESET, philo->died_at - philo->ate_at);
-	else if (philo->died && !philo->ate_at)
-		printf(BLUE "%ld\n" RESET, philo->died_at - philo->began_at);
-	else if (philo->ate_at)
 	{
-		if (philo->stopped_at > philo->ate_at)
-			printf(BLUE "%ld\n" RESET, philo->stopped_at - philo->ate_at);
-		else
-			printf(BLUE "%ld\n" RESET, 0L);
+		printf(BLUE "%ld\n" RESET, philo->died_at - philo->ate_at);
+		return ;
 	}
-	else
+	if (philo->died && !philo->ate_at)
+	{
+		printf(BLUE "%ld\n" RESET, philo->died_at - philo->began_at);
+		return ;
+	}
+	if (!philo->ate_at)
+	{
 		printf(BLUE "%ld\n" RESET, philo->stopped_at - philo->began_at);
+		return ;
+	}
+	if (philo->stopped_at > philo->ate_at)
+	{
+		printf(BLUE "%ld\n" RESET, philo->stopped_at - philo->ate_at);
+		return ;
+	}
+	printf(BLUE "%ld\n" RESET, 0L);
 }
 
 static void	print_configuration(t_simulation *s)
@@ -69,9 +91,9 @@ static void	print_configuration(t_simulation *s)
 	eat_interval = s->philos[0].timer->eat_interv;
 	sleep_interval = s->philos[0].timer->sleep_interv;
 	printf(GREY "\nCONFIG ====\n" RESET);
-	printf("Philo Count: " BLUE "%02ld" RESET " | ", s->philos_num);
-	printf("death interval: " BLUE "%4d" RESET " | ", death_interval);
-	printf("eat interval: " BLUE "%4d" RESET " | ", eat_interval);
-	printf("sleep interval: " BLUE "%4d" RESET " | ", sleep_interval);
+	printf("Philos: " BLUE "%02ld" RESET " | ", s->philos_num);
+	printf("death interval: " BLUE "%-4d" RESET " | ", death_interval);
+	printf("eat interval: " BLUE "%-4d" RESET " | ", eat_interval);
+	printf("sleep interval: " BLUE "%-4d" RESET " | ", sleep_interval);
 	printf("must eat: " BLUE "%d\n" RESET, s->meals_goal);
 }
